861ac624e66d1c043691bf4cb08449cb75fac322
phamgiakiet01 <phamgiakiet01@gmail.com>
2023-12-18 11:19:14 +0700

    code 1

---
+++ Nguon/KhoiTao/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KhoiTao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("chuong trinh tinh don gian");
+            int a = 1;
+            int b = 2;
+            Console.WriteLine("a + b = {0} ", a+b);
+            Console.ReadLine();
+        }
+    }
+}
